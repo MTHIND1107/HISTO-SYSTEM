ee07b9aaabeff22e0ed1efca26409d8a2eacbc7e
Manreet Thind <Mthind2315@conestogac.on.ca>
2025-04-05 09:32:19 -0400

    Added common.h  that defines the letter range and other shared constants

---
+++ common/inc/common.h
@@ -0,0 +1,12 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+/* Constants */
+#define MIN_LETTER 'A'
+#define MAX_LETTER 'T'
+#define LETTER_RANGE (MAX_LETTER - MIN_LETTER + 1)
+
+/* Random letter generation function */
+char generate_random_letter();
+
+#endif /* COMMON_H */
